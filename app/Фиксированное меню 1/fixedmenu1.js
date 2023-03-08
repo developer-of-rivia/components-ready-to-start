d8453d06379c1d4bb34ba0a593d152a8b5e2e9c7
oloref <samping2money@gmail.com>
2023-03-08 12:59:25 +0700

    сделано фиксированное меню

---
+++ app/Фиксированное меню 1/fixedmenu1.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    /* */
+    let burgerIcon = document.querySelector('.burger-icon');
+    let closeIcon = document.querySelector('.menu__close');
+    let menu = document.querySelector('.menu');
+    let scrollObject = document.querySelector('.menu');
+
+    burgerIcon.addEventListener('click', function(){
+        menu.classList.add('menu--open');
+        scrollLock.disablePageScroll(scrollObject);
+    })
+    closeIcon.addEventListener('click', function(){
+        menu.classList.remove('menu--open');
+        scrollLock.enablePageScroll(scrollObject);
+    })
+
+
+    window.onscroll = function showHeader() {
+        var header = document.querySelector('.header');
+        if(window.pageYOffset > 100){
+            header.classList.add('header_fixed');
+        } else{
+            header.classList.remove('header_fixed');
+        }
+    }
+    /* */
+}, false);
